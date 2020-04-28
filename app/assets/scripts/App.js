f1a39b57dbe68ca2d7d04bf2fbad13873dbf7780
Temuulen Enkhbold <eb.temuulen@gmail.com>
2020-04-28 21:05:00 +0800

    Desktop and Mobile header done

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,6 +1,23 @@
 // import "../styles/styles.css";
 import "../styles/styles.scss";
+import MobileMenu from "./modules/MobileMenu";
+// import Person from "./modules/Person";
 
 if (module.hot) {
   module.hot.accept();
 }
+
+let mobileMenu = new MobileMenu();
+
+// class Adult extends Person {
+//   paytaxes() {
+//     console.log(this.name + " no tax balance");
+//   }
+// }
+
+// var john = new Person("John Doe", "green");
+// john.greet();
+
+// var jane = new Adult("Jane Doe", "pink");
+// jane.greet();
+// jane.paytaxes();

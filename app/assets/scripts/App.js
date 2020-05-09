3f4f9a220b6f546aab0d13966e1ced6bc942cd1a
Temuulen Enkhbold <eb.temuulen@gmail.com>
2020-05-09 15:56:03 +0800

    modal overlay done

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -3,6 +3,8 @@ import "../styles/styles.scss";
 import MobileMenu from "./modules/MobileMenu";
 import RevealOnScroll from "./modules/RevealOnScroll";
 import StickyHeader from "./modules/StickyHeader";
+import { type } from "os";
+// import Modal from "./modules/Modal";
 // import Person from "./modules/Person";
 
 if (module.hot) {
@@ -10,11 +12,29 @@ if (module.hot) {
 }
 
 // let revealOnScroll = new RevealOnScroll();
+// new Modal();
 let stickyHeader = new StickyHeader();
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
 new RevealOnScroll(document.querySelectorAll(".testimonial"), 70);
 
 let mobileMenu = new MobileMenu();
+let modal;
+
+document.querySelectorAll(".open-modal").forEach(el => {
+  el.addEventListener("click", e => {
+    e.preventDefault();
+    if (typeof modal == "undefined") {
+      import(/* webpackChunkName: "modal" */ "./modules/Modal")
+        .then(x => {
+          modal = new x.default();
+          setTimeout(() => modal.openModal(), 20);
+        })
+        .catch(() => console.log("error"));
+    } else {
+      modal.openModal();
+    }
+  });
+});
 
 // class Adult extends Person {
 //   paytaxes() {
